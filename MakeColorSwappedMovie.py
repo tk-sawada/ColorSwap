e84d51597e3cc7d7482913724ce927aaa2694bc8
tk-sawada <130136436+tk-sawada@users.noreply.github.com>
2023-05-25 13:43:02 +0900

    Update MakeColorSwappedMovie.py

--- MakeColorSwappedMovie.py
+++ MakeColorSwappedMovie.py
@@ -6,7 +6,37 @@ from tkinter import filedialog, ttk, Toplevel, Button, Label, messagebox, simple
 from moviepy.editor import VideoFileClip, AudioFileClip
 
 def process_frame(img):
-    # 画像処理のコード（省略）
+    # 画像をBGRからLABに変換
+    img_lab = cv2.cvtColor(img, cv2.COLOR_BGR2Lab)
+
+    # LAB色空間をチャネルごとに分割
+    l_channel, a_channel, b_channel = cv2.split(img_lab)
+
+    # AとBのチャネルを反転
+    a_channel_inv = cv2.bitwise_not(a_channel)
+    b_channel_inv = cv2.bitwise_not(b_channel)
+
+    # 反転したチャネルと元のLチャネルを合成
+    img_lab_inv = cv2.merge([l_channel, a_channel_inv, b_channel_inv])
+        
+    # LABからBGRに戻す
+    img_bgr_inv = cv2.cvtColor(img_lab_inv, cv2.COLOR_Lab2BGR)
+
+    # 画像をBGRからRGBに変換
+    img_rgb_inv = cv2.cvtColor(img_bgr_inv, cv2.COLOR_BGR2RGB)
+
+    # RGB色空間をチャネルごとに分割
+    r_channel, g_channel, b_channel = cv2.split(img_rgb_inv)
+
+    # RGB各色のチャネルに対してヒストグラム均等化を適用
+    r_eq = cv2.equalizeHist(r_channel)
+    g_eq = cv2.equalizeHist(g_channel)
+    b_eq = cv2.equalizeHist(b_channel)
+
+    # 均等化したチャネルを結合
+    img_rgb_eq = cv2.merge([r_eq, g_eq, b_eq])
+
+    return img_rgb_eq
 
 root = tkinter.Tk()
 root.withdraw()
